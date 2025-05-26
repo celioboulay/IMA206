0a459d893eb714f5166e07c984e1cb7a0c34fc3b
Celio Boulay <123871877+Surimi91@users.noreply.github.com>
2025-05-26 20:54:18 +0200

    fixed import + tqdm

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,14 +4,16 @@ import glob
 from PIL import Image
 import torchvision.transforms as transforms
 import torch
-
+from tqdm import tqdm 
 
 class Data_loader(object):
 
     def __init__(self, file_paths):
         self.file_paths = file_paths
         self.images = []
-        self.transform = transforms.Compose([
+
+
+        self.transform = transforms.Compose([ # a changer bien sur 
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -19,23 +21,15 @@ class Data_loader(object):
         ])
 
     def load_images(self):
-        for file_path in self.file_paths:
+        for file_path in tqdm(self.file_paths, desc="Loading images"):
             try:
                 self.images.append(Image.open(file_path).convert('RGB'))
             except Exception as e:
                 print(e)
-        return self.images
-
-
-    def load_folder(self, folder_path):
-        self.file_paths = glob.glob(f'{folder_path}/*.jpg')
-        return self.load_images()
-
+        #return self.images
 
     def display_image(self, im):
-        if isinstance(im, torch.Tensor):
-            im = transforms.ToPILImage()(im)
-        im.show()
+        Image._show(im)
 
 
     def extract_patch(self, image, method=None):
@@ -46,15 +40,13 @@ class Data_loader(object):
         return patch
 
 
+all_files_paths = glob.glob('/Users/celio/Documents/database/**/*.jpg', recursive=True)
 
-
-file_paths = glob.glob('/Users/celio/Documents/database/afro - afro-basaldella_1912/*.jpg')
-data_loader = Data_loader(file_paths)
+data_loader = Data_loader(all_files_paths[:600])
 loaded_images = data_loader.load_images()
-print(data_loader.images[0])
+print(data_loader.images[0].size)
 
 
-if loaded_images:
-    im = data_loader.images[0]
-    patch = data_loader.extract_patch(im)
-    data_loader.display_image(patch)
+im = data_loader.images[0]
+patch = data_loader.extract_patch(im)
+data_loader.display_image(im)
