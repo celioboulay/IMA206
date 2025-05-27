7f581d3e82e6c8b9ec9f6158b4b771d23468dfdd
Celio Boulay <123871877+Surimi91@users.noreply.github.com>
2025-05-27 20:40:28 +0200

    structure_et_datasets

--- learning_models/learning_model.py
+++ learning_models/learning_model.py
@@ -7,3 +7,30 @@ sys.path.insert(0, parent_dir)
 
 
 import numpy as np
+from utils.data_loader import *    # notamment Custom loader dataset
+import pandas as pd
+
+
+
+annotations_file = '/Users/celio/Documents/Classeur1.csv'
+img_dir = '/Users/celio/Documents/smalldata'
+annotations_df = pd.read_csv(annotations_file, sep=';')
+
+
+train_df, test_df = train_test_split(annotations_df, test_size=0.2, 
+    stratify=annotations_df.iloc[:, 1],  # colonne labels
+    random_state=42) # random state pour les tests
+
+
+train_dataset = CustomDataset(train_df, img_dir)
+test_dataset = CustomDataset(test_df, img_dir)
+
+
+im, label = test_dataset[20]
+print(im.shape, ' ', label)
+
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+
+
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -7,22 +7,21 @@ sys.path.insert(0, parent_dir)
 
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
 from PIL import Image
 import torch
 from tqdm import tqdm
-import os
 import torchvision
 import pandas as pd
 from torchvision.io import read_image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+from sklearn.model_selection import train_test_split
 import utils.transformations_init
 
 
 class CustomDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
+    def __init__(self, annotations_df, img_dir, transform=None, target_transform=None):
+        self.img_labels = annotations_df.reset_index(drop=True)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -32,15 +31,36 @@ class CustomDataset(Dataset):
     
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
+        image = read_image(img_path.replace(';0', ''))
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
-            image = self.transform(image) # en fonction du patch que l'on veut
+            image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
-    
 
 
-small_dataset = CustomDataset('/Users/celio/Documents/Classeur1.csv', '/Users/celio/Documents/smalldata')
-print(small_dataset.__len__())
+######## A voir pour implementer un héritage CustomDataloader(Dataloader) pour extraire les patchs qu'on veut cf partie de Jean ?
+
+annotations_file = '/Users/celio/Documents/Classeur1.csv'
+img_dir = '/Users/celio/Documents/smalldata'
+annotations_df = pd.read_csv(annotations_file, sep=';')
+
+
+train_df, test_df = train_test_split(annotations_df, test_size=0.2, 
+    stratify=annotations_df.iloc[:, 1],  # colonne labels
+    random_state=42) # random state pour les tests
+
+
+train_dataset = CustomDataset(train_df, img_dir)
+test_dataset = CustomDataset(test_df, img_dir)
+
+
+im, label = test_dataset[20]
+print(im.shape, ' ', label)
+
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+
+
