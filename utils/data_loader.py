23119bb3864e833088c6b6c40c84b1893da1f3a0
Celio Boulay <123871877+Surimi91@users.noreply.github.com>
2025-05-27 11:25:31 +0200

    data_loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -5,12 +5,15 @@ from PIL import Image
 import torch
 from tqdm import tqdm 
 import transformations_init
+import os
+import torchvision
 
 class Data_loader(object):
 
     def __init__(self, file_paths, method):
         self.file_paths = file_paths
         self.images = []  # interet de pas direct charger les images ?
+        self.dict = {}
         self.transform = method
 
 
@@ -18,13 +21,23 @@ class Data_loader(object):
         for file_path in tqdm(self.file_paths, desc="Loading images"):
             try:
                 self.images=[]  # reset pour eviter les problemes
-                self.images.append(Image.open(file_path).convert('RGB'))
+                self.images.append(torchvision.transforms.functional.pil_to_tensor(Image.open(file_path).convert('RGB')))
+
+                base_name = os.path.basename(file_path)
+                image_keys=[]
+                image_keys.append(os.path.splitext(base_name)[0])
+
             except Exception as e:
                 print(e)
+
+        self.dict = dict(zip(image_keys, self.images))
         #return self.images
 
+
+
     def display_image(self, im):
-        Image._show(im)
+        #Image._show(im)
+        pass
 
 
     def extract_patch(self, image, method=None):
@@ -38,13 +51,12 @@ class Data_loader(object):
 
 
 
-all_files_paths = glob.glob('/Users/celio/Documents/database/**/*.jpg', recursive=True)
+all_files_paths = glob.glob('/Users/celio/Documents/database/**/*.jpg', recursive=True) # extrait de la meme maniere, mais pas dans l'ordre naif
 
-data_loader = Data_loader(all_files_paths[:600], method=transformations_init.transform_pas_ouf)
-loaded_images = data_loader.load_images()
-print(data_loader.images[0].size)
+data_loader = Data_loader(all_files_paths[:20], method=transformations_init.transform_pas_ouf)
+data_loader.load_images()
+
+print(data_loader.dict['bouguereau_9'])
 
 
 im = data_loader.images[0]
-patch = data_loader.extract_patch(im)
-data_loader.display_image(im)
