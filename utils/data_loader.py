771948e2d2a7524eb7bc96cb9f1bd600bffe0219
Celio Boulay <123871877+Surimi91@users.noreply.github.com>
2025-05-27 19:01:41 +0200

    torch loader + smaller data for activation viz

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -13,67 +13,34 @@ import torch
 from tqdm import tqdm
 import os
 import torchvision
+import pandas as pd
+from torchvision.io import read_image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import utils.transformations_init
 
 
-class ArtDataset(Dataset):
-    def __init__(self, file_paths, transform=None):
-        self.file_paths = file_paths
+class CustomDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
         self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.file_paths)
-
+        return len(self.img_labels)
+    
     def __getitem__(self, idx):
-        img_path = self.file_paths[idx]
-        base_name = os.path.basename(img_path)
-        image_key = os.path.splitext(base_name)[0]
-
-        try:
-            image = Image.open(img_path).convert('RGB')
-        except Exception as e:
-            print(e)
-            return None, image_key
-
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
         if self.transform:
-            image = self.transform(image)
-
-        return image, image_key
-
-
-class Data_loader:
-    def __init__(self, file_paths, method):
-        self.file_paths = file_paths
-        self.transform = method
-        self.art_dataset = ArtDataset(file_paths=self.file_paths, transform=self.transform)
-        self.images = []
-        self.dict = {}
-        self._load_all_images_to_memory()
+            image = self.transform(image) # en fonction du patch que l'on veut
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+    
 
 
-    def _load_all_images_to_memory(self):
-        for idx in tqdm(range(len(self.art_dataset)), desc="Loading images"):
-            img_tensor, image_key = self.art_dataset[idx]
-            if img_tensor is not None:
-                self.images.append(img_tensor)
-                self.dict[image_key] = img_tensor
-
-    def display_image(self, im):
-        pass
-
-    def extract_patch(self, image, method=None):
-        if method is None:
-            method = self.transform
-        patch = method(image)
-        print(patch.shape)
-        return patch
-
-all_files_paths = glob.glob('/Users/celio/Documents/database/**/*.jpg', recursive=True)
-
-data_loader = Data_loader(all_files_paths[:20], method=utils.transformations_init.transform_pas_ouf)
-
-print(data_loader.dict['bouguereau_9'])
-
-im = data_loader.images[0]
+small_dataset = CustomDataset('/Users/celio/Documents/Classeur1.csv', '/Users/celio/Documents/smalldata')
+print(small_dataset.__len__())
