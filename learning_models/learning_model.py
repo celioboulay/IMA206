8835f8cc76bc41e611f46cfff6be17558d871b77
Surimi91 <celio.boulay@outlook.fr>
2025-05-29 16:00:46 +0200

    Adapt code for CUDA execution

--- learning_models/learning_model.py
+++ learning_models/learning_model.py
@@ -8,11 +8,19 @@ sys.path.insert(0, parent_dir)
 import numpy as np
 from utils.data_loader import *    # notamment Custom loader dataset
 import pandas as pd
+from Models.sequentials import SimpleCNN
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Subset, random_split
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 
 
-annotations_file = '/Users/celio/Documents/Classeur1.csv'
-img_dir = '/Users/celio/Documents/smalldata'
+annotations_file = 'Data/Classeur1.csv'
+img_dir = 'Data/smalldata'
 annotations_df = pd.read_csv(annotations_file, sep=';')
 
 
@@ -21,8 +29,16 @@ train_df, test_df = train_test_split(annotations_df, test_size=0.2,
     random_state=42) # random state pour les tests
 
 
-train_dataset = CustomDataset(train_df, img_dir)
-test_dataset = CustomDataset(test_df, img_dir)
+
+# Normalisation à appliquer sur le DataLoader
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
+])
+
+train_dataset = CustomDataset(train_df, img_dir, transform=transform)
+test_dataset = CustomDataset(test_df, img_dir, transform=transform)
 
 
 
@@ -30,14 +46,50 @@ train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
 
 
-images, labels = next(iter(train_loader))
 
-batch_size = 4
-fig, axes = plt.subplots(1, batch_size, figsize=(12, 4))
-for i in range(batch_size):
-    img = images[i].permute(1, 2, 0).numpy() # torch tensors de forme [C, H, W] mais matplotlib veut [H, W, C] pour l'affichage
-    ax = axes[i]
-    ax.imshow(img)
-    ax.set_title(f"Label: {train_dataset.classes[labels[i]]}")
-    ax.axis('off')
-plt.show()
+
+nb_channels = 3
+nb_classes = 3
+learning_rate = 1e-3
+n_epochs = 25
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(torch.cuda.get_device_name(0))
+
+model = SimpleCNN(nb_channels, nb_classes).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+######################################## Entrainement du modele
+
+train_loss = []
+
+for epoch in range(n_epochs):
+    model.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+
+    for data, target in tqdm(train_loader, desc=f'Epoch {epoch+1}/{n_epochs}'):
+        data, target = data.to(device), target.to(device) # pour avoir les donnees et le modele sur le meme device
+
+
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, target)
+        loss.backward()  # computes dloss/dx for every parameter x which has requires_grad=True
+        optimizer.step() # updates the value of x using x.grad
+
+        train_loss += loss.item()
+
+        _, predicted = torch.max(outputs, 1)
+        correct += (predicted==target).sum().item()
+        total += target.size(0)
+
+    train_accuracy = correct / total
+    avg_train_loss = train_loss / len(train_loader)
+
+    print(f'Epoch {epoch+1}, Train Loss: {avg_train_loss:.4f}, Train Acc: {train_accuracy:.4f}')
+
+
+
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -30,13 +30,15 @@ class CustomDataset(Dataset):
         return len(self.img_labels)
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path.replace(';0', ''))
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0].replace(';0', ''))
+        image = Image.open(img_path).convert("RGB")  
         label = self.img_labels.iloc[idx, 1]
+
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
+
         return image, label
 
 
