d41a3a54a4c6e8534659aa6e987b0349ada1df25
Surimi91 <celio.boulay@outlook.fr>
2025-06-01 21:50:59 +0200

    :)

--- Models/SAE.py
+++ Models/SAE.py
@@ -1,2 +1,30 @@
-class SAE(object):
-    pass
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+
+
+class SAE(nn.Module): # pour l'instant jsp si faut vraiemnt appeler Stacked AE mais vsy
+    def __init__(self, latent_dim=32):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 100, 5),
+            nn.MaxPool2d(2),
+            nn.Conv2d(100, 200, 5),
+            nn.MaxPool2d(2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.MaxUnpool2d(2),
+            nn.ConvTranspose2d(200, 100, 5),
+            nn.MaxUnpool2d(2),
+            nn.ConvTranspose2d(200, 100, 5)
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
+    
+
+# Trying the just defined SAE 
--- Models/sequentials.py
+++ Models/sequentials.py
@@ -1,46 +1,6 @@
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
-from sklearn.model_selection import train_test_split
-
-
-
-class SimpleCNN(nn.Module):
-    def __init__(self, nb_channels, nb_classes):
-        super(SimpleCNN, self).__init__()
-        self.features = nn.Sequential(
-
-            nn.Conv2d(nb_channels, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(32, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128 * (256 // 8) * (256 // 8), 64),
-            nn.ReLU(),
-            nn.Dropout(0.4),
-            nn.Linear(64, nb_classes)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        return x
-    
-
-
 
 
 class CNN_2(nn.Module):
@@ -76,42 +36,6 @@ class CNN_2(nn.Module):
     
     def forward(self, x):
         x = self.features(x)
-        x = self.classifier(x)
-        return x
-    
-
-
-
-resnet = models.resnet50(pretrained=True)
-
-features_extractor = nn.Sequential(  # On récupère le début du ResNet jusqu'à layer3
-    resnet.conv1,
-    resnet.bn1,
-    resnet.relu,
-    resnet.maxpool,
-    resnet.layer1,
-    resnet.layer2,
-    resnet.layer3
-)
-for param in features_extractor.parameters():
-    param.requires_grad = False
-
-class CNN3(nn.Module):
-    def __init__(self, nb_classes, base_model=features_extractor):
-        super().__init__()
-        self.features = base_model
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(1024, 256),  # 1024 car layer3 sort du 1024 channels
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, nb_classes)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.pool(x)
         x = self.classifier(x)
         return x
     
