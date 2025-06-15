a5e6cacab42a17c56c289d2a18dd07d03bc57f40
Surimi91 <celio.boulay@outlook.fr>
2025-06-15 13:29:15 +0200

    fixed merge_embeddings

--- global_features/compute_global.py
+++ global_features/compute_global.py
@@ -16,19 +16,14 @@ from init_global import init
 from simCLR_like import global_SSL
 
 
-def compute_features(dataset, datasetmodel, device): # dans compute_global parce que peut etre ca depend de la scale
-    pass
-
-
 
 def process_features(features_tensor):  # pour ramener les features extraites a la forme choisie, a savoir un .pt pour le moment
-    
-    embeddings=None
-
+    embeddings=features_tensor
+    print(embeddings.shape())
     return embeddings
 
 
-def compute(data_path, embedding_dir, device):
+def compute(data_path, device, embedding_dir=current_dir):
 
     dataset = datasets.ImageFolder(data_path, transform=transform_local_center)
     dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
@@ -38,8 +33,8 @@ def compute(data_path, embedding_dir, device):
     f_theta_2.to(device)
     f_theta_2.eval()
 
-    features_tensor = init(dataloader, device, model=f_theta_2)
+    # global SLL args a passer dans compute
     f_theta_2 = global_SSL(data_path, device, f_theta_2, n_epochs=20, batch_size=32, lr=1e-3, weight_decay=1e-6, temperature=0.5)  # data_path parce quon va charger un dataset different
-    features_tensor = compute_features(dataset, f_theta_2, device)
+    features_tensor = init(dataloader, device, model=f_theta_2)
     embeddings = process_features(features_tensor)
     torch.save(embeddings, embedding_dir+"/global_embeddings.pt")
--- utils/merge_embeddings.py
+++ utils/merge_embeddings.py
@@ -1,16 +1,26 @@
 import os
+import torch
+
+def merge(embeddings_dir, output_file, dim=0):
+
+    tensors = [f for f in os.listdir(embeddings_dir) if f.endswith('.pt')]
+    if len(tensors) != 2: raise ValueError('please compute embeddings before clustering')
+    tensors.sort()
+
+    tensor_list = [torch.load(os.path.join(embeddings_dir, f)) for f in tensors]
 
 
-def merge(embeddings_dir):
-    if len(os.listdir(embeddings_dir)) == 0: raise Exception('please compute embeddings before clustering')
-    
+
+    '''
+    f_alpha = load_model(...)
+
+    boucle dec
+
+    tant que pas implemente on utilisera
+    embeddings = cat(z1, z2)
+    '''
+
+    merged_tensor = torch.cat(tensor_list, dim=dim)
 
 
-    #f_alpha = load_model(...)
-
-    #boucle dec
-
-
-    #tant que pas implemente on utilisera
-    #embeddings = concatenate(z1, z2)
-    #return embeddings
+    torch.save(merged_tensor, output_file)
