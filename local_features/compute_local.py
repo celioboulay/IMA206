7bd819b715058fdc2b9fc56801f7fd36f57ac87e
JeanGrng <jgroeninger@proton.me>
2025-06-19 16:34:40 +0200

    compute local

--- local_features/compute_local.py
+++ local_features/compute_local.py
@@ -23,4 +23,62 @@ def extract_patch(image):
 
 
 def compute(data_path, embedding_dir, device) :
-    pass
+
+    #On initialise notre modèle
+    device = torch.device('cuda')
+    vae_model = VanillaVAE(in_channels=1, latent_dim=64, kld_weight=1e-3).to(device)
+    checkpoint = torch.load("./vae_checkpoint_epoch4_batch44345.pt", map_location=torch.device('cuda'))
+    vae_model.load_state_dict(checkpoint['model_state_dict'])
+    vae_model.eval()
+    
+    with torch.no_grad :
+    #On prend toutes les images une par une pour en extraire les patchs
+        
+        image_extensions = (".jpg", ".jpeg", ".png")
+
+        for root, dirs, files in os.walk(data_path):
+            for file in files:
+                if file.lower().endswith(image_extensions):
+                    image_path = os.path.join(root, file)
+                    print(f"Found image: {image_path}")
+
+                    #Extraction des patchs de l'image
+                    image = cv2.imread(data_path)
+                    if image is None:
+                        print(f"Could not read image: {data_path}") 
+
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+                    patch_scores, selected_patches = extract_patch(image)
+
+                    #Passage dans la VAE
+                    embedding = []
+
+                    for patch in selected_patches:
+                        
+                        patch_tensor = torch.tensor(patch).unsqueeze(0).unsqueeze(0).float().to(device)
+
+                        z, _ = vae_model.encode(patch_tensor) 
+                        z = z.squeeze(0)  
+
+                        embedding.append(z)
+
+                    # Concatène tous les embedding de patchs
+                    merged_embedding = torch.cat(embedding, dim=0)  # shape: (5*d,)
+
+
+                    # On sauvegarde l'embedding dans le dossier spécifié
+                    embedding_path = os.path.join(embedding_dir, f"{os.path.splitext(file)[0]}_embedding.pt")
+                    torch.save(merged_embedding, embedding_path)
+
+
+
+if __name__ == "__main__":
+    data_path = "../Data"
+    embedding_dir = "../embeddings/local"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    if not os.path.exists(embedding_dir):
+        os.makedirs(embedding_dir)
+
+    compute(data_path, embedding_dir, device)
